4173ce71143254ff4cd019d3c87283661dc1fdeb
Mingtian Yang <mky5089@psu.edu>
2019-09-03 19:26:07 -0400

    add actions and logics for generating word and display the word label

--- WordScramble/WordScramble/ViewController.swift
+++ WordScramble/WordScramble/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let DEFULT_WORD_SIZE:Int! = 4
+    let wordModel = WordModel()
     @IBOutlet weak var WordDisplayLabel: UILabel!
     @IBOutlet weak var ResultLabel: UILabel!
     @IBOutlet weak var WordSegments: UISegmentedControl!
@@ -19,9 +20,65 @@ class ViewController: UIViewController {
     @IBOutlet weak var NewWordButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.resetAll()
+        
         // Do any additional setup after loading the view.
     }
+    
+    func resetAll() {
+        //initialize UI element status
+        CheckButton.isEnabled = false
+        UndoButton.isEnabled = false
+        WordDisplayLabel.text = " "
+        WordSegments.removeAllSegments()
+    }
 
+    @IBAction func NewWordOnClick(_ sender: Any) {
+        var currentWordSize: Int!
+        
+        //generate new word based on selected word size
+        if let wordSizeTitle = ChooseWordLengthSegment.titleForSegment(at: ChooseWordLengthSegment.selectedSegmentIndex) {
+             currentWordSize = Int(wordSizeTitle)
+        } else {
+            currentWordSize = DEFULT_WORD_SIZE
+        }
+        wordModel.setCurrentWordSize(newSize: currentWordSize)
+        updateWordSegmentWithNewWord(newWord: wordModel.randomWord)
+        
+        //print(wordModel.randomWord)
+    }
+    
+    func updateWordSegmentWithNewWord(newWord: String!) {
+        var charArray = [Character](newWord)
+        charArray.shuffle();
+        //reset the segment
+        WordSegments.removeAllSegments()
+        
+        //Display the shuffled characters in the word segment
+        for (index, char) in charArray.enumerated() {
+            WordSegments.insertSegment(withTitle: (String)(char), at: index, animated: true)
+        }
+    }
+    
+    func updateWordLabelWithNewChar(newChar: Character!) {
+        if let currentWordDisplayLabelText = WordDisplayLabel.text {
+            WordDisplayLabel.text = currentWordDisplayLabelText + String(newChar)
+        }
+    }
+    
+    @IBAction func CharOnChoose(_ sender: Any) {
+        var choosedChar: Character!
+        
+        //generate new word based on selected word size
+        if let segTitle = WordSegments.titleForSegment(at: WordSegments.selectedSegmentIndex) {
+            choosedChar = segTitle.first
+        }
+        
+        //let the segment selet process act like a button click
+        WordSegments.selectedSegmentIndex = -1
+        //print(choosedChar!)
+        updateWordLabelWithNewChar(newChar: choosedChar!)
 
+    }
 }
 
