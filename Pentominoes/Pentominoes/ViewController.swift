4f47dc2b56654a516811097903a33a930709359a
Mingtian Yang <mky5089@psu.edu>
2019-09-11 12:56:37 -0400

    initialize assignment 3

---
+++ Pentominoes/Pentominoes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pentominoes
+//
+//  Created by Mingtian Yang on 9/11/19.
+//  Copyright © 2019 Mingtian Yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
