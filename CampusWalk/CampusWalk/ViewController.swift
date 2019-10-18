58b20a9f201b45efd09d95fd242bcc9a867e1974
Mingtian Yang <mky5089@psu.edu>
2019-10-18 18:06:22 -0400

    create assignment 8

---
+++ CampusWalk/CampusWalk/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CampusWalk
+//
+//  Created by Mingtian Yang on 10/18/19.
+//  Copyright © 2019 Mingtian Yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
