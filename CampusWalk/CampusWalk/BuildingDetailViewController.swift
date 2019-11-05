9a47cafbab87205e85be83c55e6d811f90ccaf29
Mingtian Yang <mky5089@psu.edu>
2019-11-05 10:46:36 -0500

    updates to details view

--- CampusWalk/CampusWalk/BuildingDetailViewController.swift
+++ CampusWalk/CampusWalk/BuildingDetailViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 
-class BuildingDetailViewController: UIViewController, UITextViewDelegate {
+class BuildingDetailViewController: UIViewController, UITextViewDelegate, ImagePickerDelegate {
     
     var scrollView:UIScrollView!
+    var imagePicker: ImagePicker!
     var building: Building!
     var customizeImage: UIImage?
+    var imageView: UIImageView!
     var buildingDetail: BuildingDetail!
     var descriptionTextview: UITextView!
     var keyboardHeight:CGFloat = 0
@@ -28,10 +30,12 @@ class BuildingDetailViewController: UIViewController, UITextViewDelegate {
         center.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
         center.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
         self.setEditing(true, animated: true)
+        self.imagePicker = ImagePicker(presentationController: self, delegate: self)
         // Do any additional setup after loading the view.
     }
     
     override func viewDidLayoutSubviews() {
+        print("viewDidLayoutSubviews")
         let size = self.view.bounds.size
         let scrollviewFrame = CGRect(x: 0, y: 0, width: size.width, height: size.height)
         self.scrollView = UIScrollView(frame: scrollviewFrame)
@@ -50,38 +54,70 @@ class BuildingDetailViewController: UIViewController, UITextViewDelegate {
             buildingYearLabel.text = ""
         }
         
-        if (!self.building.photo.isEmpty) {
+        imageView = UIImageView(frame: CGRect(x: 20, y: 80, width: size.width-40, height: size.width-40))
+        
+        if let image = self.customizeImage {
+            let imageViewFrame = CGRect(x: 20, y: 80, width: size.width-40, height: size.width-40)
+            imageView = UIImageView(frame: imageViewFrame)
+            imageView.image = image
+        } else if let image = self.buildingDetail.customizeImage {
+            let imageViewFrame = CGRect(x: 20, y: 80, width: size.width-40, height: size.width-40)
+            imageView = UIImageView(frame: imageViewFrame)
+            imageView.image = image
+        } else if (!self.building.photo.isEmpty) {
             let image = UIImage(named: "\(self.building.photo).jpg")
             let imageViewFrame = CGRect(x: 20, y: 80, width: size.width-40, height: size.width-40)
-            let imageView = UIImageView(frame: imageViewFrame)
+            imageView = UIImageView(frame: imageViewFrame)
             imageView.image = image
-            self.scrollView.addSubview(imageView)
-            
-            let descriptionTextviewFrame = CGRect(x: 20, y: imageView.frame.origin.y  + imageView.frame.size.height, width: size.width-40, height: 40)
-                        
-            self.descriptionTextview = UITextView(frame: descriptionTextviewFrame)
-            self.descriptionTextview.delegate = self
-            descriptionTextview.font = .systemFont(ofSize: 20)
-            descriptionTextview.isScrollEnabled = false
-            if (self.buildingDetail.description.isEmpty) {
-                descriptionTextview.text = "Enter Descriptions Here"
-                descriptionTextview.textColor = UIColor.lightGray
-            } else {
-                descriptionTextview.text = self.buildingDetail.description
-                descriptionTextview.textColor = UIColor.black
-            }
-            self.resizeDescriptionTextView()
-            descriptionTextview.keyboardType = .asciiCapable
-            descriptionTextview.keyboardDismissMode = .onDrag
-            self.scrollView.addSubview(descriptionTextview)
-
         }
+        
+        self.scrollView.addSubview(imageView)
+
+        let buttonFrame = CGRect(x: 20, y: 80 + imageView.frame.size.height + 20, width: size.width-40, height: 40)
+        let button = UIButton(frame: buttonFrame)
+        button.backgroundColor = .black
+        button.setTitle("Select Image or Take Photo", for: .normal)
+        button.addTarget(self, action: #selector(selectPhoto(sender:)), for: .touchUpInside)
+
+        self.scrollView.addSubview(button)
+
+        let descriptionTextviewFrame = CGRect(x: 20, y: imageView.frame.origin.y  + imageView.frame.size.height + buttonFrame.height + 40, width: size.width-40, height: 40)
+        
+        self.descriptionTextview = UITextView(frame: descriptionTextviewFrame)
+        self.descriptionTextview.delegate = self
+        descriptionTextview.font = .systemFont(ofSize: 20)
+        descriptionTextview.isScrollEnabled = false
+        if (self.buildingDetail.description.isEmpty) {
+            descriptionTextview.text = "Enter description here"
+            descriptionTextview.textColor = UIColor.lightGray
+        } else {
+            descriptionTextview.text = self.buildingDetail.description
+            descriptionTextview.textColor = UIColor.black
+        }
+        self.resizeDescriptionTextView()
+        descriptionTextview.keyboardType = .asciiCapable
+        descriptionTextview.keyboardDismissMode = .onDrag
+        self.scrollView.addSubview(descriptionTextview)
         
         self.scrollView.addSubview(buildingNameLabel)
         self.scrollView.addSubview(buildingYearLabel)
         
 
         self.view.addSubview(scrollView)
+    }
+    
+    @objc func selectPhoto(sender: UIButton!) {
+      self.imagePicker.present(from: sender)
+    }
+    
+    func didSelect(image: UIImage?) {
+        if let selectedImage = image {
+            self.customizeImage = selectedImage
+            self.imageView.image = selectedImage
+//            self.view.setNeedsLayout()
+//            self.scrollView.setNeedsLayout()
+            self.updateBuilding()
+        }
     }
     
     func initialize(building: Building, buildingDetail: BuildingDetail) {
@@ -152,7 +188,11 @@ class BuildingDetailViewController: UIViewController, UITextViewDelegate {
     }
     
     func updateBuilding() {
-        self.buildingDetail.description = self.descriptionTextview.text
+        if (self.descriptionTextview.text == "Enter description here") {
+            self.buildingDetail.description = ""
+        } else {
+            self.buildingDetail.description = self.descriptionTextview.text
+        }
         self.buildingDetail.customizeImage = self.customizeImage
         mapModel.updateBuildingDetail(building: self.building, newBuildingDetail: self.buildingDetail)
     }
