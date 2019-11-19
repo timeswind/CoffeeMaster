11fb438368a35467b54c95cd517be388247d2564
Mingtian Yang <mky5089@psu.edu>
2019-11-18 19:54:15 -0500

    construct coffee brew step file

---
+++ CoffeeMaster/CoffeeMaster/BrewMethod.swift
@@ -0,0 +1,69 @@
+//
+//  BrewMethod.swift
+//  CoffeeMaster
+//
+//  Created by Mingtian Yang on 11/18/19.
+//  Copyright © 2019 Mingtian Yang. All rights reserved.
+//
+
+import Foundation
+
+enum GrindSizeType: String {
+    case Coarse = "Coarse"
+    case Medium = "Medium"
+    case Fine = "Fine"
+    case ExtraFine = "ExtraFine"
+    case Turkish = "Turkish"
+}
+
+enum WeightUnit: String {
+    case g = "g"
+    case oz = "oz"
+}
+
+enum BrewStepType: String {
+    case Coffee = "Coffee"
+    case Water = "Water"
+}
+
+struct BrewMethod:Decodable {
+    var name: String
+    var image: String
+    var description: String
+}
+
+class BrewStep {
+    var brewType: BrewStepType!
+    var weightAmount: Int = 0
+    var weightUnit: WeightUnit
+    
+    init(brewType: BrewStepType, weightUnit: WeightUnit) {
+        self.brewType = brewType
+        self.weightUnit = weightUnit
+    }
+    
+}
+
+class BrewGuide {
+    var guideName: String = ""
+    var guideDescription: String = ""
+    var isPublic: Bool = false
+    var baseBrewMethod: BrewMethod!
+    private var brewSteps: [BrewStep] = []
+    
+    init(baseBrewMethod: BrewMethod) {
+        self.baseBrewMethod = baseBrewMethod
+    }
+    
+    func getBrewSteps() -> [BrewStep] {
+        return self.brewSteps
+    }
+    
+    func add(brewStep: BrewStep) {
+        self.brewSteps.append(brewStep)
+    }
+    
+    func removeBrewStep(at index: Int) {
+        self.brewSteps.remove(at: index)
+    }
+}
