ecd096fdf0d5aeb1c87566b8c6378566594fa348
Mingtian Yang <mky5089@psu.edu>
2019-12-15 16:40:59 -0500

    improvement(today'swidget): finished layout today widget extention

--- CoffeeMaster/Coffee Master Widget/WidgetView.swift
+++ CoffeeMaster/Coffee Master Widget/WidgetView.swift
@@ -18,19 +18,57 @@ struct WidgetView: View {
     }
     
     var body: some View {
-        VStack {
+        VStack(alignment: .leading, spacing: 0) {
+            Button(action: {self.openURL(urlString: "coffeemaster://brew")}) {
+                HStack {
+                    Image("HarioV60-icon").resizable()
+                    .aspectRatio(contentMode: .fit).frame(height: 22)
+                    
+                    Text("Brew Coffee").font(.caption).foregroundColor(Color.white).fontWeight(.bold)
+                    }.padding(8).background(Color.Theme.Accent)
+                .cornerRadius(10)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color(.sRGB, red: 150/255, green: 150/255, blue: 150/255, opacity: 0.1), lineWidth: 1)
+                )
+                
+            }.buttonStyle(PlainButtonStyle())
+            
             HStack {
-                Button(action: {self.openURL(urlString: "coffeemaster://brew")}) { Text("Open Brew View") }
-                Button(action: {self.openURL(urlString: "coffeemaster://record")}) { Text("Open Record View") }
+                Button(action: {self.openURL(urlString: "coffeemaster://record.note")}) {
+                    HStack {
+                        
+                        Image("record-icon-select-100").resizable()
+                        .aspectRatio(contentMode: .fit).frame(width: 22).foregroundColor(Color.white)
+                        Text("Record Note").font(.caption).foregroundColor(Color.white).fontWeight(.bold)
+                        }.padding(8).background(Color.Theme.Accent)
+                    .cornerRadius(10)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color(.sRGB, red: 150/255, green: 150/255, blue: 150/255, opacity: 0.1), lineWidth: 1)
+                    )
+                }.padding(.vertical, 4)
+                Button(action: {self.openURL(urlString: "coffeemaster://record.caffeine")}) {
+                    HStack {
+                        Image("icons-coffee-beans-500").resizable()
+                        .aspectRatio(contentMode: .fit).frame(height: 22)
+                        
+                        Text("Record Caffeine").font(.caption).foregroundColor(Color.white).fontWeight(.bold)
+                        }.padding(8).background(Color.Theme.Accent)
+                    .cornerRadius(10)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color(.sRGB, red: 150/255, green: 150/255, blue: 150/255, opacity: 0.1), lineWidth: 1)
+                    )
+                }.buttonStyle(PlainButtonStyle())
             }
-            Button(action: {self.openURL(urlString: "coffeemaster://record.caffeine")}) { Text("RecordCaffeine") }
-            Spacer()
-        }
+
+        }.padding().background(Color.clear)
     }
 }
 
 struct WidgetView_Previews: PreviewProvider {
     static var previews: some View {
-        WidgetView()
+        WidgetView().previewLayout(.sizeThatFits)
     }
 }
